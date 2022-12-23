d1058f674210f1618a2e3081ab23f30cee224af9
Nauman Sajid <e0389102@u.nus.edu>
2022-12-24 00:30:24 +0800

    Implementation of Selection Sort

---
+++ DataStructuresAndAlgorithms/src/nauman/dsa/algorithms/sorting/SelectionSort.java
@@ -0,0 +1,34 @@
+package nauman.dsa.algorithms.sorting;
+
+/** 
+ * Implementation of Selection Sort Algorithm
+ * 
+ * Properties:
+ * Space-Complexity: O(1) therefore In-Place
+ * Time-Complexity: O(n^2)
+ * Stable Sort
+ * 
+ * @author     Nauman
+ * @version	   1.0
+ */
+
+public class SelectionSort	 {
+	
+	public static int [] sort(int[] input) {
+		int smallest, temp;
+		for (int i = 0; i < input.length-1; i ++) {
+			smallest = i;
+			for (int j = i+1; j < input.length; j++) {
+				if (input[j] < input[smallest]) {
+					smallest = j;
+				}
+			}
+			temp = input[i];
+			input[i] = input[smallest];
+			input[smallest] = temp;
+		}
+		
+		return input;
+	}
+	
+}
