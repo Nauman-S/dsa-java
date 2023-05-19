10b10a689f5b3e1ec97001c68e4fb6f76096b7bc
Nauman Sajid <e0389102@u.nus.edu>
2023-05-19 23:35:42 +0800

    Updated HashMap Implementation

--- DataStructuresAndAlgorithms/src/nauman/dsa/dataStructures/HashMap.java
+++ DataStructuresAndAlgorithms/src/nauman/dsa/dataStructures/HashMap.java
@@ -1,33 +1,18 @@
 package nauman.dsa.dataStructures;
 
-import java.math.BigInteger;
+import java.util.Objects;
 
-public class HashMap {
+public class HashMap <K,V> {
 	
 	/*
      * Implementation notes.
-     *
-     * This map uses Multiplicative Hashing to generate the hash of a number
-     * 
-     * m = number of buckets
-     * p = A large fixed prime number such that p >= m
-     * For the purpose of this implementation we take p as being 2147483647 = 2^31 - 1 (which happens to be prime) 
-     * 
-     * Random Integer a in range [1,p-1]
-     * Random Integer b in range [0,p-1]
-     * 
-     * h(c) = hash of Integer c
-     * h(c) = ((a*c + b) mod p) mod m
-     * 
-     * Can be proven that for fixed m,p and randomly chosen a,b the probabilitiy of hash collision is 1/m 
-     * Hence, its a Universal Hash Function
      * 
      * 
      */
 	
 	private static final int DEFAULT_INITIAL_CAPACITY = 16;
 	
-	private static final int PRIME = 2147483647;
+	static final float DEFAULT_LOAD_FACTOR = 0.75f;
 	
 	private int currentCapacity;
 	
@@ -36,7 +21,7 @@ public class HashMap {
      */
 	private int size;
 	
-	private Node [] arr;
+	private Node<K,V> [] arr;
 	
 	public HashMap() {
 		this.currentCapacity = DEFAULT_INITIAL_CAPACITY;
@@ -53,15 +38,28 @@ public class HashMap {
 	public void rehash(int newCapacity) {
 		currentCapacity = newCapacity;
 		
-		Node [] oldNodes = arr;
+		Node<K,V> [] oldNodes = arr;
 		arr = new Node [currentCapacity];
 		
-		for (Node node: oldNodes) {
-			Node current = node;
+		for (Node<K,V> node: oldNodes) {
+			Node<K,V> current = node;
 			while (current != null) {
-				put(current.key, current.value);
+				int index = getIndex(current.hash); 
+				Node<K,V> n1 = arr[index];
+				Node<K,V> np = null;
+				while (n1 != null) {
+					np = n1;
+					n1 = n1.next;
+				}
+				if (np == null) {
+					arr[index] = new Node<K,V> (current.hash, current.key, current.value,null);
+				} else {
+					np.next = new Node<K,V> (current.hash, current.key, current.value,null);
+				}
+	
 			}
 		}
+		
 		
 	}
 	
@@ -72,14 +70,15 @@ public class HashMap {
      * @param key the key to be found
      * @return the value, or null if not found
      */
-	public void put(int key, int value) {
-		int index = getHash(key);
-		Node current = arr[index];
-		Node prev = null;
+	public void put(K key, V value) {
+		int hash = key.hashCode();
+		int index = getIndex(hash);
+		Node<K,V> current = arr[index];
+		Node<K,V> prev = null;
 		
 		while (current != null) {
 			if (current.key == key) {
-				current.value = value;
+				current.setValue(value);
 				return;
 			}
 			prev = current;
@@ -87,17 +86,18 @@ public class HashMap {
 		}
 		
 		if (prev == null) {
-			arr[index] = new Node(key, value);
+			arr[index] = new Node<K,V> (hash, key, value,null);
 		} else {
-			prev.next = new Node(key, value);
+			prev.next = new Node<K,V> (hash, key, value, null);
 		}
 		
 		size++;
 		
-		if (size > currentCapacity) {
+		if ((float)size/currentCapacity > DEFAULT_LOAD_FACTOR) {
 			rehash(currentCapacity*2);
 		}
 	}
+	
 	
     /**
      * Hashes key, finds it in bucket and returns its value if found
@@ -105,11 +105,11 @@ public class HashMap {
      * @param key the key to be found
      * @return the value, or null if not found
      */
-	public Integer get(int key) {
-		Node current = arr[getHash(key)];
+	public V get(K key) {
+		Node<K,V> current = arr[getIndex(key.hashCode())];
 		while (current != null) {
-			if (current.key == key) {
-				return current.value;
+			if (current.getKey().equals(key)) {
+				return current.getValue();
 			}
 			current = current.next;
 		}
@@ -123,13 +123,13 @@ public class HashMap {
      * @param key the key to be found
      * @return the value, or null if not found
      */
-	public void remove(int key) {
-		int index = getHash(key);
-		Node current = arr[index];
-		Node prev = null;
+	public V remove(K key) {
+		int index = getIndex(key.hashCode());
+		Node <K,V> current = arr[index];
+		Node <K,V> prev = null;
 		
 		while (current != null) {
-			if (current.key == key) {
+			if (current.getKey().equals(key)) {
 				break;
 			}
 			prev = current;
@@ -143,32 +143,51 @@ public class HashMap {
 			} else {
 				arr[index] = current.next;
 			}
+			return current.getValue();
 		}
+		return null;
 	}
 	
+	public int getIndex(int hashCode) {
+		int hash = hashCode ^ (hashCode >>> 16);
+		int index = (currentCapacity - 1) & hash;
 
-	//Assume is universal hash function; Is not really
-	public int getHash(int key) {
-		BigInteger c = BigInteger.valueOf(key);
-		BigInteger p = BigInteger.valueOf(PRIME);
-		BigInteger m = BigInteger.valueOf(currentCapacity);
-		BigInteger index = c.multiply(p).mod(m);
-		return index.intValue();
+		return index;
 	}
 	
-//	public int getHash(int key) {
-//		return Integer.hashCode(key) % currentCapacity;
-//	}
+	//return Objects.hashCode(key) % currentCapacity;
+
 	
-	private class Node {
-		Node next;
-		int key;
-		int value;
-		Node(int key, int value) {
+	static class Node <K,V> {
+		final int hash;
+		final K key;
+		V value;
+		Node<K,V> next;
+		Node(int hash, K key, V value, Node<K,V> next) {
+			this.hash = key.hashCode();
 			this.key = key;
 			this.value = value;
 			this.next = null;
 		}
+		public K getKey() {
+			return key;
+		}
+		
+		public V getValue() {
+			return value;
+		}
+		
+		public void setValue(V value) {
+			this.value = value;
+		}
+		
+		public void setNext( Node <K,V> next) {
+			this.next = next;
+		}
+	}
+	
+	public int size() {
+		return size;
 	}
 	
 }
